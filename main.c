e4ae09914dbaff72c4e1958839d012e87c859a00
litiansuo <2296923560@qq.com>
2019-01-17 14:57:01 +0800

    加入删除系统

--- main.c
+++ main.c
@@ -38,6 +38,19 @@ int main()
         }
         if (code == 2)
         {
+            if(index>0){
+                index--;
+                printf("shanchuchenggong,dianjihuichejixu\n");
+                char xx;
+                scanf("%c",&xx);
+                scanf("%c",&xx);
+            }
+            else{
+                printf("wucheliangweizhixinxi,shanchushibai,dianjihuichejixu\n");
+                char xx;
+                scanf("%c",&xx);
+                scanf("%c",&xx);
+            }
         }
         if (code == 3)
         {
